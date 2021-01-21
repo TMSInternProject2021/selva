1b4193d3712ce5cc2d93ce5dfc23dbfc37bd7669
Selva Prince <selvaprince@Selvas-MacBook-Pro.local>
2021-01-21 15:56:11 +0530

    first commit

--- TMSInternProject/src/test/java/com/quinbay/TMS/Action/ValidateTimeFieldAction.java
+++ TMSInternProject/src/test/java/com/quinbay/TMS/Action/ValidateTimeFieldAction.java
@@ -1,2 +1,36 @@
-package com.quinbay.TMS.Action;public class ValidateTimeFieldAction {
+package com.quinbay.TMS.Action;
+
+import com.quinbay.TMS.Pages.Add_New_Connection_Page;
+import com.quinbay.TMS.Pages.ValidateTimeFieldPage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public class ValidateTimeFieldAction {
+
+    public ValidateTimeFieldPage validatePage;
+    public ValidateTimeFieldAction(WebDriver driver){
+        validatePage= PageFactory.initElements(driver, ValidateTimeFieldPage.class);
+    }
+    public  void click_addnew_action(){
+        validatePage.click_Addnew();
+
+    }
+
+
+    public  void enter_details_action() throws InterruptedException {
+        validatePage.origin_hub();
+        validatePage.des_hub();
+        validatePage.date();
+        validatePage.repeat();
+        validatePage.start_time();
+        validatePage.transit_time();
+        validatePage.vehicle_Type();
+
+
+    }
+
+
+    public  void select_save_action() throws InterruptedException {
+        validatePage.click_Save();
+    }
 }
