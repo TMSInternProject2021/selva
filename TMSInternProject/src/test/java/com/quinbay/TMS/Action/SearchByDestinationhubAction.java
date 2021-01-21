71988328501884fb7327089fb288577ce628f2b5
Selva Prince <selvaprince@Selvas-MacBook-Pro.local>
2021-01-21 15:58:48 +0530

    first commit

--- TMSInternProject/src/test/java/com/quinbay/TMS/Action/SearchByDestinationhubAction.java
+++ TMSInternProject/src/test/java/com/quinbay/TMS/Action/SearchByDestinationhubAction.java
@@ -1,2 +1,26 @@
-package com.quinbay.TMS.Action;public class SearchByDestinationhubAction {
+package com.quinbay.TMS.Action;
+
+import com.quinbay.TMS.Pages.LoginPage1;
+import com.quinbay.TMS.Pages.SearchByDestinationhubPage;
+import com.quinbay.TMS.Pages.SortPage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public class SearchByDestinationhubAction {
+
+    SearchByDestinationhubPage searchByDestinationhubPage;
+
+
+
+    public SearchByDestinationhubAction(WebDriver driver) {
+        searchByDestinationhubPage= PageFactory.initElements(driver, SearchByDestinationhubPage.class);
+    }
+
+    public void click_Des_Hub_Action(){
+        searchByDestinationhubPage.select_Des_Hub();
+    }
+    public void click_Hub_Action() throws InterruptedException {
+        searchByDestinationhubPage.select_DestinationHub();
+    }
+
 }
