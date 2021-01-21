f4a478310000d9270da53a84678b2f439269e925
Selva Prince <selvaprince@Selvas-MacBook-Pro.local>
2021-01-21 15:52:33 +0530

    first commit

--- TMSInternProject/src/test/java/com/quinbay/TMS/Pages/SortPage.java
+++ TMSInternProject/src/test/java/com/quinbay/TMS/Pages/SortPage.java
@@ -1,2 +1,24 @@
-package com.quinbay.TMS.Pages;public class SortPage {
+package com.quinbay.TMS.Pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class SortPage {
+
+    WebDriver driver;
+
+    public  SortPage(WebDriver webDriver){
+        driver=webDriver;
+    }
+
+    @FindBy(xpath = "(//div//span[@class='icon-down-arrow down'])[2]")
+    WebElement descending_sort;
+
+    public void descending_Sort() throws InterruptedException {
+        Thread.sleep(1000);
+
+        descending_sort.click();
+
+    }
 }
