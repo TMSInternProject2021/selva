062cdc498c59321d7d1f4aba41c7ca518d014516
Selva Prince <selvaprince@Selvas-MacBook-Pro.local>
2021-01-21 15:57:30 +0530

    first commit

--- TMSInternProject/src/test/java/com/quinbay/TMS/Action/SortAction.java
+++ TMSInternProject/src/test/java/com/quinbay/TMS/Action/SortAction.java
@@ -1,2 +1,18 @@
-package com.quinbay.TMS.Action;public class SortAction {
+package com.quinbay.TMS.Action;
+
+import com.quinbay.TMS.Pages.Add_New_Connection_Page;
+import com.quinbay.TMS.Pages.SortPage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public class SortAction {
+    public SortPage Sort_page;
+    public SortAction(WebDriver driver){
+        Sort_page= PageFactory.initElements(driver, SortPage.class);
+    }
+
+    public void sort_Descending_action() throws InterruptedException {
+        Sort_page.descending_Sort();
+    }
+
 }
