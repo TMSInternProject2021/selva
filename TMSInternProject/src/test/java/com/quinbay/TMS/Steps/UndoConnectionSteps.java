3e32da2a34695dbeb44631130d95329ef5b3ea60
Selva Prince <selvaprince@Selvas-MacBook-Pro.local>
2021-01-21 15:47:19 +0530

    first commit

--- TMSInternProject/src/test/java/com/quinbay/TMS/Steps/UndoConnectionSteps.java
+++ TMSInternProject/src/test/java/com/quinbay/TMS/Steps/UndoConnectionSteps.java
@@ -1,2 +1,46 @@
-package com.quinbay.TMS.Steps;public class UndoConnectionSteps {
+package com.quinbay.TMS.Steps;
+
+import com.quinbay.TMS.Action.Add_New_Connection_Action;
+import com.quinbay.TMS.Action.UndoConnectionAction;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.WebDriver;
+
+public class UndoConnectionSteps {
+    WebDriver driver;
+    public UndoConnectionAction undoConnectionAction;
+    public Add_New_Connection_Action add_new_connection_action;
+
+
+    @Given("User will be on the linehaul details page")
+    public void userWillBeOnTheLinehaulDetailsPage() {
+        driver=LoginSteps1.driver;
+        undoConnectionAction= new UndoConnectionAction(driver);
+        add_new_connection_action=new Add_New_Connection_Action(driver);
+        System.out.println("User will be on the linehaul details page");
+    }
+
+    @And("The new connection must be created by user")
+    public void theNewConnectionMustBeCreatedByUser() throws InterruptedException {
+        add_new_connection_action.enterDetailsForUndoAction();
+    }
+    @When("User Clicks on the delete icon")
+    public void userClicksOnTheDeleteIcon() throws InterruptedException {
+        undoConnectionAction.del_Connection_Action();
+    }
+
+    @And("Select the undo option")
+    public void selectTheUndoOption() {
+        undoConnectionAction.undo_Connection_Action();
+    }
+
+    @Then("Validate whether the connection has been restored.")
+    public void validateWhetherTheConnectionHasBeenRestored() {
+
+        System.out.println("The connection has been restored");
+    }
+
+
 }
