511b9d3c9ce6843097a0f992308b8e8c92d82b19
Selva Prince <selvaprince@Selvas-MacBook-Pro.local>
2021-01-21 15:47:47 +0530

    first commit

--- TMSInternProject/src/test/java/com/quinbay/TMS/Steps/SortSteps.java
+++ TMSInternProject/src/test/java/com/quinbay/TMS/Steps/SortSteps.java
@@ -1,2 +1,31 @@
-package com.quinbay.TMS.Steps;public class SortSteps {
+package com.quinbay.TMS.Steps;
+
+import com.quinbay.TMS.Action.Add_New_Connection_Action;
+import com.quinbay.TMS.Action.SortAction;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.WebDriver;
+
+public class SortSteps {
+    WebDriver driver;
+    public SortAction sortAction;
+
+    @Given("User must be on the linehaul deails page")
+    public void userMustBeOnTheLinehaulDeailsPage() {
+
+        driver=LoginSteps1.driver;
+        sortAction= new SortAction(driver);
+        System.out.println("You are on the Linehaul detils web page");
+    }
+
+    @When("User click the destination hub down arrow")
+    public void userClickTheDestinationHubDownArrow() throws InterruptedException {
+        sortAction.sort_Descending_action();
+    }
+
+    @Then("User be able to see the descending sorted linehaul details")
+    public void theUserWillBeNavigatedToTheResultPage() {
+        System.out.println("The user be able to see the descending sorted linehaul details");
+    }
 }
