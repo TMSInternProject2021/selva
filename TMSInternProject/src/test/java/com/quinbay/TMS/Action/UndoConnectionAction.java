39d145e844ee7865cd535503287a5e5b307aacbe
Selva Prince <selvaprince@Selvas-MacBook-Pro.local>
2021-01-21 15:56:36 +0530

    first commit

--- TMSInternProject/src/test/java/com/quinbay/TMS/Action/UndoConnectionAction.java
+++ TMSInternProject/src/test/java/com/quinbay/TMS/Action/UndoConnectionAction.java
@@ -1,2 +1,20 @@
-package com.quinbay.TMS.Action;public class UndoConnectionAction {
+package com.quinbay.TMS.Action;
+
+import com.quinbay.TMS.Pages.UndoConnectionPage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public class UndoConnectionAction {
+
+    public UndoConnectionPage undoConnectionPage;
+    public UndoConnectionAction(WebDriver driver){
+        undoConnectionPage= PageFactory.initElements(driver, UndoConnectionPage.class);
+    }
+
+    public void del_Connection_Action() throws InterruptedException {
+        undoConnectionPage.del_connection();
+    }
+    public void undo_Connection_Action(){
+        undoConnectionPage.undo_connection();
+    }
 }
