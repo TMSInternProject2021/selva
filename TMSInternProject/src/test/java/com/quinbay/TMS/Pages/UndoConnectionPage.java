f880a4789e101356e71191a588aea7914102e909
Selva Prince <selvaprince@Selvas-MacBook-Pro.local>
2021-01-21 15:51:53 +0530

    first commit

--- TMSInternProject/src/test/java/com/quinbay/TMS/Pages/UndoConnectionPage.java
+++ TMSInternProject/src/test/java/com/quinbay/TMS/Pages/UndoConnectionPage.java
@@ -1,2 +1,33 @@
-package com.quinbay.TMS.Pages;public class UndoConnectionPage {
+package com.quinbay.TMS.Pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class UndoConnectionPage {
+
+    WebDriver driver;
+
+    public UndoConnectionPage(WebDriver webDriver){
+        driver=webDriver;
+    }
+
+    @FindBy(xpath = "//div//tr[1]//img[@class='icon margin-l10']")
+    WebElement delete_icon;
+
+    @FindBy(xpath = "//div//button[@class='undo-button']")
+    WebElement undo_button;
+    public void del_connection() throws InterruptedException {
+        delete_icon.click();
+        Thread.sleep(3000);
+
+    }
+    public  void undo_connection(){
+        undo_button.click();
+
+    }
+
+
+
+
 }
