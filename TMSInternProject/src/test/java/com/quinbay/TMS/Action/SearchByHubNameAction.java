bcc0468c0273cf88133b2545bf164a37fd0ded48
Selva Prince <selvaprince@Selvas-MacBook-Pro.local>
2021-01-21 15:58:18 +0530

    first commit

--- TMSInternProject/src/test/java/com/quinbay/TMS/Action/SearchByHubNameAction.java
+++ TMSInternProject/src/test/java/com/quinbay/TMS/Action/SearchByHubNameAction.java
@@ -1,2 +1,22 @@
-package com.quinbay.TMS.Action;public class SearchByHubNameAction {
+package com.quinbay.TMS.Action;
+
+import com.quinbay.TMS.Pages.SearchByHubNamePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public class SearchByHubNameAction {
+
+    public SearchByHubNamePage searchByHubNamePage;
+
+    public SearchByHubNameAction(WebDriver webDriver){
+        searchByHubNamePage= PageFactory.initElements(webDriver,SearchByHubNamePage.class);
+    }
+
+    public void enter_HubName_Action() throws InterruptedException {
+        searchByHubNamePage.enter_HubName();
+    }
+
+    public void click_Search_Icon_Action(){
+        searchByHubNamePage.click_Search_Icon();
+    }
 }
