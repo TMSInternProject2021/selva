80809f1f80e89fb73228189c0d6f54f7cb2d4498
Selva Prince <selvaprince@Selvas-MacBook-Pro.local>
2021-01-21 15:46:21 +0530

    first commit

--- TMSInternProject/src/test/java/com/quinbay/TMS/Steps/ValidateTimeFieldSteps.java
+++ TMSInternProject/src/test/java/com/quinbay/TMS/Steps/ValidateTimeFieldSteps.java
@@ -1,2 +1,38 @@
-package com.quinbay.TMS.Steps;public class ValidateTimeFieldSteps {
+package com.quinbay.TMS.Steps;
+
+import com.quinbay.TMS.Action.ValidateTimeFieldAction;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.WebDriver;
+
+public class ValidateTimeFieldSteps {
+
+    WebDriver driver;
+    public ValidateTimeFieldAction validateTimeFieldAction;
+
+    @Given("The user must be on the Add new connection page")
+    public void theUserMustBeOnTheAddNewConnectionPage() {
+        driver=LoginSteps1.driver;
+        validateTimeFieldAction= new ValidateTimeFieldAction(driver);
+        validateTimeFieldAction.click_addnew_action();
+    }
+
+    @When("User enter invalid values in the start time field")
+    public void userEnterInvalidValuesInTheStartTimeField() throws InterruptedException {
+        validateTimeFieldAction.enter_details_action();
+
+    }
+
+    @And("User clicks on the save button")
+    public void userClicksOnTheSaveButton() throws InterruptedException {
+        validateTimeFieldAction.select_save_action();
+    }
+
+    @Then("Mandatory error message will be displayed")
+    public void mandatoryErrorMessageWillBeDisplayed() {
+
+        System.out.println("Validation of time field is tested----'Mandatory error message will be displayed'");
+    }
 }
