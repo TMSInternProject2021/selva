ef3980c7172eb9853e6b397a27fe44007dd02349
Selva Prince <selvaprince@Selvas-MacBook-Pro.local>
2021-01-21 15:59:24 +0530

    first commit

--- TMSInternProject/src/test/java/com/quinbay/TMS/Action/LoginAction1.java
+++ TMSInternProject/src/test/java/com/quinbay/TMS/Action/LoginAction1.java
@@ -1,2 +1,29 @@
-package com.quinbay.TMS.Action;public class LoginAction1 {
+package com.quinbay.TMS.Action;
+
+//import com.quinbay.TMS.Pages.LoginPage;
+import com.quinbay.TMS.Pages.LoginPage1;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginAction1 {
+    LoginPage1 loginPage1;
+    public LoginAction1(WebDriver driver) {
+        loginPage1= PageFactory.initElements(driver, LoginPage1.class);
+    }
+
+
+    public void login_Action() throws InterruptedException {
+       // Thread.sleep(3000);
+        loginPage1.enter_UserCredentials();
+    }
+
+    public void select_Hub_Action() throws InterruptedException {
+        loginPage1.select_Hub();
+    }
+    public void select_Settings_Action(){
+        loginPage1.select_Settings();
+    }
+    public void navigation_To_linehaul_settings(){
+        loginPage1.navigation_ToLinehaulDetails();
+    }
 }
