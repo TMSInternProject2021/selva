1120ae31213c420a67c2000ede80d116e7a098be
Selva Prince <selvaprince@Selvas-MacBook-Pro.local>
2021-01-22 10:26:02 +0530

    first commit

--- TMSInternProject/src/main/java/Pages/SearchByHubNamePage.java
+++ TMSInternProject/src/main/java/Pages/SearchByHubNamePage.java
@@ -1,5 +1,6 @@
 package Pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -29,9 +30,30 @@ public class SearchByHubNamePage {
     @FindBy(xpath = "//div//button")
     WebElement click_search_icon;
 
+    @FindBy(xpath = "//div//input[@class='input-box-style pull-left search-bar']")
+    WebElement searchText;
+
+    @FindBy(xpath = "//tr//td[1]")
+    WebElement org_result;
+    @FindBy(xpath = "//tr//td[2]")
+    WebElement des_result;
     public void click_Search_Icon()
     {
         click_search_icon.click();
+    }
+
+    public boolean verifySearchByHubName(){
+        System.out.println(searchText.getText());
+        if(org_result.getText().equals(searchText.getText()) || des_result.getText().equals(searchText.getText())){
+            return true;
+        }
+        else
+        {
+            return  false;
+        }
+
+
+
     }
 
 
