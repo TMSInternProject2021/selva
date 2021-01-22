374e28206bb3c8c786a9a20cfb4c926c82f651cb
Selva Prince <selvaprince@Selvas-MacBook-Pro.local>
2021-01-22 13:24:22 +0530

    first commit

--- TMSInternProject/src/main/java/Pages/SearchByHubNamePage.java
+++ TMSInternProject/src/main/java/Pages/SearchByHubNamePage.java
@@ -19,32 +19,41 @@ public class SearchByHubNamePage {
     @FindBy(xpath = "//div[@class='option']")
     WebElement select_hub;
 
+    String searchText;
     public void enter_HubName() throws InterruptedException {
         searchbar.click();
         Thread.sleep(3000);
         searchbar.sendKeys("TMS Hub");
+
         Thread.sleep(1000);
+        searchText=select_hub.getText();
         select_hub.click();
     }
 
     @FindBy(xpath = "//div//button")
     WebElement click_search_icon;
 
-    @FindBy(xpath = "//div//input[@class='input-box-style pull-left search-bar']")
-    WebElement searchText;
+   // @FindBy(xpath = "//div//input[@class='input-box-style pull-left search-bar']")
+
 
     @FindBy(xpath = "//tr//td[1]")
     WebElement org_result;
     @FindBy(xpath = "//tr//td[2]")
     WebElement des_result;
+
+
+    //div//span[@class='overlay-text']--failed
+    //div//span[@class='icon-cross pull-right cursor-pointer']--cross
     public void click_Search_Icon()
     {
         click_search_icon.click();
     }
 
     public boolean verifySearchByHubName(){
-        System.out.println(searchText.getText());
-        if(org_result.getText().equals(searchText.getText()) || des_result.getText().equals(searchText.getText())){
+//       System.out.println(searchText);
+//        System.out.println(org_result.getText());
+//        System.out.println(des_result.getText());
+        if(org_result.getText().equals(searchText) || des_result.getText().equals(searchText)){
             return true;
         }
         else
@@ -52,15 +61,8 @@ public class SearchByHubNamePage {
             return  false;
         }
 
-
+//        return true;
 
     }
-
-
-
-
-
-
-
 
 }
