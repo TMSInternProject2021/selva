999ab64cd6ccdf743e03a17cc9758ee9271fa5c4
Selva Prince <selvaprince@Selvas-MacBook-Pro.local>
2021-01-22 10:26:56 +0530

    first commit

--- TMSInternProject/src/main/java/Pages/SearchByDestinationhubPage.java
+++ TMSInternProject/src/main/java/Pages/SearchByDestinationhubPage.java
@@ -1,11 +1,13 @@
 package Pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-public class SearchByDestinationhubPage {
+public class SearchByDestinationhubPage<selected_hub> {
     WebDriver driver;
+    //WebElement selected_hub;
 
     public  SearchByDestinationhubPage(WebDriver webDriver){
         driver=webDriver;
@@ -14,24 +16,50 @@ public class SearchByDestinationhubPage {
     @FindBy(xpath = "//label[text()='Destination hub']")
     WebElement click_destinationhub;
 
-           // (//div//span[@class='flex-box'])[2]//label[@class='clickable']
-
-
     public void select_Des_Hub(){
         click_destinationhub.click();
     }
     @FindBy(xpath = "//div//div//label[contains(text(),'TMS Hub')]")
-            //div[@class='hub-dropdown']//div[@class='option unchecked'][43]
     WebElement select_hub;
 
     @FindBy(xpath = "//div[@class='margin-b10']")
 
     WebElement click_out;
+    @FindBy(xpath = "//div[@class='option']")
+    public WebElement selected_hub;
+
+    //selected_hub=driver.findElement(By.xpath("//div[@class='option']"));
+String x="";
+
+
+
+
 
     public void select_DestinationHub() throws InterruptedException {
         select_hub.click();
         Thread.sleep(3000);
+        x=selected_hub.getText();
         click_out.click();
+    }
+
+    @FindBy(xpath = "//tr//td[1]")
+    WebElement org_result;
+    @FindBy(xpath = "//tr//td[2]")
+    WebElement des_result;
+
+    public boolean verifySearchByDestinationhub(){
+
+
+        if(org_result.getText().equals(x) || des_result.getText().equals(x)){
+            return true;
+        }
+        else
+        {
+            return  false;
+        }
+
+
+
     }
 
 }
